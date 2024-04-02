c7ab3d9987ad9dadd028a71f591c3b9b0577faaa
OrangeLiuSSE <orangeliusse@163.com>
2024-04-02 19:51:20 +0800

    modify consumer

--- backend/backend/consumer.py
+++ backend/backend/consumer.py
@@ -1,7 +1,17 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import os
 import asyncio
 from backend.image_preprocesser import image_to_base64
+import os
+
+def list_files(directory):
+    image_list = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            image_list.append(os.path.join(root, file))
+    
+    return image_list
 
 class ImageConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -10,21 +20,19 @@ class ImageConsumer(AsyncWebsocketConsumer):
         # await self.run_model_and_send_images()
 
     async def run_model_and_send_images(self):
-        for step in range(1, 1001):  # 假设模型运行1000步
+        image_list = list_files('data/image')
+        image_list.sort()
+        cnt = 0
+        for step in range(1, 41):  # 假设模型运行1000步
             # 模拟模型运行所需时间
             await asyncio.sleep(0.05)  # 使用asyncio.sleep来模拟异步运行
-            if step == 100:
-                image_data = image_to_base64('data/image/nus_logo.png')
+            if step % 20 == 0:
+                image_data = image_to_base64(image_list[cnt])
                 await self.send(text_data=json.dumps({
                     'image': image_data,
                     'step': step
                 }))
-            elif step == 200:  
-                image_data = image_to_base64('data/image/ntu_logo.webp')
-                await self.send(text_data=json.dumps({
-                    'image': image_data,
-                    'step': step
-                }))
+                cnt += 1
             else:
                 await self.send(text_data=json.dumps({
                     'step': step
